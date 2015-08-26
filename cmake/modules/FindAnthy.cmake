e09ecf2b34aa2b70c23171556dbb42039e534546
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-26 11:11:05 +0800

    detect anthy

--- cmake/modules/FindAnthy.cmake
+++ cmake/modules/FindAnthy.cmake
@@ -1,1 +1,13 @@
 option(ENABLE_ANTHY "disable Anthy input method [default: auto-detect]" TRUE)
+
+if(NOT ${ENABLE_ANTHY})
+    return()
+endif()
+
+
+pkg_check_modules(ANTHY anthy)
+
+if (NOT ANTHY_FOUND)
+    message(WARNING "Cannot find anthy library, set ENABLE_ANTHY to FALSE")
+    set(ENABLE_ANTHY FALSE)
+endif()
