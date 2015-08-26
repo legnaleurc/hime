5e7b215c2c0d44a7596127414d5fa899fe3cf971
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-26 11:17:09 +0800

    detect gtk2 im module

--- cmake/modules/FindGtk2IMModule.cmake
+++ cmake/modules/FindGtk2IMModule.cmake
@@ -1,1 +1,13 @@
 option(ENABLE_GTK2_IM_MODULE "enable GTK+ 2.x im-module [default: auto-detect]" TRUE)
+
+if(NOT ${ENABLE_GTK2_IM_MODULE})
+    return()
+endif()
+
+
+find_package(GTK2)
+
+if (NOT GTK2_FOUND)
+    message(WARNING "Cannot find gtk2 library, set ENABLE_GTK2_IM_MODULE to FALSE")
+    set(ENABLE_GTK2_IM_MODULE FALSE)
+endif()
