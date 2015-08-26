39ead0a5f738333b372fc2ce2a32047430da2413
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-26 11:19:24 +0800

    detect gtk3 im module

--- cmake/modules/FindGtk3IMModule.cmake
+++ cmake/modules/FindGtk3IMModule.cmake
@@ -1,1 +1,13 @@
 option(ENABLE_GTK3_IM_MODULE "enable GTK+ 3.x im-module [default: auto-detect]" TRUE)
+
+if(NOT ${ENABLE_GTK3_IM_MODULE})
+    return()
+endif()
+
+
+pkg_check_modules(GTK3 gtk+-3.0)
+
+if (NOT GTK3_FOUND)
+    message(WARNING "Cannot find gtk3 library, set ENABLE_GTK3_IM_MODULE to FALSE")
+    set(ENABLE_GTK3_IM_MODULE FALSE)
+endif()
