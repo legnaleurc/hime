05f1fe0b0887a384ee498d5b68b3b0fc07ac3a17
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-26 11:21:39 +0800

    detect qt3

--- cmake/modules/FindQt3IMModule.cmake
+++ cmake/modules/FindQt3IMModule.cmake
@@ -1,1 +1,13 @@
 option(ENABLE_QT3_IM_MODULE "enable QT3 im-module [default: disable]" FALSE)
+
+if(NOT ${ENABLE_QT3_IM_MODULE})
+    return()
+endif()
+
+
+find_package(Qt3)
+
+if (NOT QT_FOUND)
+    message(WARNING "Cannot find Qt3 library, set ENABLE_QT3_IM_MODULE to FALSE")
+    set(ENABLE_QT3_IM_MODULE FALSE)
+endif()
