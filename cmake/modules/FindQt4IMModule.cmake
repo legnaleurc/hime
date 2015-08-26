d3f3c14d6eeed62cb831a14cb743191ce0ac7f21
Wei-Cheng Pan <wpan@mozilla.com>
2015-08-26 11:22:36 +0800

    detect qt4

--- cmake/modules/FindQt4IMModule.cmake
+++ cmake/modules/FindQt4IMModule.cmake
@@ -1,1 +1,14 @@
 option(ENABLE_QT4_IM_MODULE "enable QT4 im-module [default: auto-detect]" TRUE)
+
+if(NOT ${ENABLE_QT4_IM_MODULE})
+    return()
+endif()
+
+
+find_package(Qt4)
+
+if (NOT QT4_FOUND)
+    message(WARNING "Cannot find Qt4 library, set ENABLE_QT4_IM_MODULE to FALSE")
+    set(ENABLE_QT4_IM_MODULE FALSE)
+endif()
+
